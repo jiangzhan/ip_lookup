c203677bdbc1c4fd31a0fc9dd421b816345899d9
Jiang Zhan <jiang@openmediafoundation.org>
2019-05-08 09:25:17 -0600

    update user agent class

--- src/UserAgent.php
+++ src/UserAgent.php
@@ -4,82 +4,86 @@ namespace Drupal\member_login;
 
 
 class UserAgent {
+  
+  protected $u_agent;
+  protected $bname;
+  protected $platform;
+  protected $version;
+
+  function __construct ($u_agent, $bname = 'Unknown', $platform = 'Unknown', $version= " ") {
+    $this->u_agent = $u_agent;
+    $this->bname = $bname;      
+    $this->platform = $platform;
+    $this->version = $version;
+  }
 
   public function getBrowser() { 
     $u_agent = $_SERVER['HTTP_USER_AGENT']; 
-    $bname = 'Unknown';
-    $platform = 'Unknown';
-    $version= "";
 
     //First get the platform?
-    if (preg_match('/linux/i', $u_agent)) {
-      $platform = 'linux';
+    if (preg_match('/linux/i', $this->u_agent)) {
+      $this->platform = 'linux';
     }
-    elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
-      $platform = 'mac';
+    elseif (preg_match('/macintosh|mac os x/i', $this->u_agent)) {
+      $this->platform = 'mac';
     }
-    elseif (preg_match('/windows|win32/i', $u_agent)) {
-      $platform = 'windows';
+    elseif (preg_match('/windows|win32/i', $this->u_agent)) {
+      $this->platform = 'windows';
     }
     
     // Next get the name of the useragent yes seperately and for good reason
-    if (preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) { 
-      $bname = 'Internet Explorer'; 
+    if (preg_match('/MSIE/i',$this->u_agent) && !preg_match('/Opera/i',$this->u_agent)) { 
+      $this->bname = 'Internet Explorer'; 
       $ub = "MSIE"; 
     } 
-    elseif (preg_match('/Firefox/i',$u_agent)) { 
-      $bname = 'Mozilla Firefox'; 
+    elseif (preg_match('/Firefox/i',$this->u_agent)) { 
+      $this->bname = 'Mozilla Firefox'; 
       $ub = "Firefox"; 
     } 
-    elseif(preg_match('/Chrome/i',$u_agent)) { 
-      $bname = 'Google Chrome'; 
+    elseif(preg_match('/Chrome/i',$this->u_agent)) { 
+      $this->bname = 'Google Chrome'; 
       $ub = "Chrome"; 
     } 
-    elseif(preg_match('/Safari/i',$u_agent)) { 
-      $bname = 'Apple Safari'; 
+    elseif(preg_match('/Safari/i',$this->u_agent)) { 
+      $this->bname = 'Apple Safari'; 
       $ub = "Safari"; 
     } 
-    elseif(preg_match('/Opera/i',$u_agent)) { 
-      $bname = 'Opera'; 
+    elseif(preg_match('/Opera/i',$this->u_agent)) { 
+      $this->bname = 'Opera'; 
       $ub = "Opera"; 
     } 
-    elseif(preg_match('/Netscape/i',$u_agent)) { 
-      $bname = 'Netscape'; 
+    elseif(preg_match('/Netscape/i',$this->u_agent)) { 
+      $this->bname = 'Netscape'; 
       $ub = "Netscape"; 
     } 
     
     // finally get the correct version number
     $known = array('Version', $ub, 'other');
-    $pattern = '#(?<browser>' . join('|', $known) . ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-    if (!preg_match_all($pattern, $u_agent, $matches)) {
-        // we have no matching number just continue
-    }
     
     // see how many we have
     $i = count($matches['browser']);
     if ($i != 1) {
       //we will have two since we are not using 'other' argument yet
       //see if version is before or after the name
-      if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
-        $version= $matches['version'][0];
+      if (strripos($this->u_agent,"Version") < strripos($this->u_agent, $ub)){
+        $this->version= $matches['version'][0];
       }
       else {
-        $version= $matches['version'][1];
+        $this->version= $matches['version'][1];
       }
     }
     else {
-      $version= $matches['version'][0];
+      $this->version= $matches['version'][0];
     }
     
     // check if we have a number
-    if ($version==null || $version=="") {$version="?";}
+    if ($this->version==null || $this->version=="") {$this->version="?";}
     
     return array(
-      'userAgent' => $u_agent,
-      'name'      => $bname,
-      'version'   => $version,
-      'platform'  => $platform,
-      'pattern'    => $pattern
+      'userAgent' => $this->u_agent,
+      'name'      => $this->bname,
+      'version'   => $this->version,
+      'platform'  => $this->platform,
     );
   } 
 }

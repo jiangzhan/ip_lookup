d6d77a492f6004781e888eb76d034d44f04bca1b
Jiang Zhan <jzzhanjiang@gmail.com>
2019-06-17 16:22:40 -0600

    Issue #3062387 by Jiang_2015: Replacing file_get_contents() with Guzzle

--- src/Resource/Resource.php
+++ src/Resource/Resource.php
@@ -5,6 +5,8 @@ namespace Drupal\ip_lookup\Resource;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Drupal\Core\Database\Connection;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
 
 /**
  * Ipdata Resource.
@@ -33,6 +35,13 @@ class Resource {
   protected $configFactory;
 
   /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
    * Constructs resource object.
    *
    * @param \Drupal\Core\Database\Connection $connection
@@ -41,11 +50,14 @@ class Resource {
    *   The request stack.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   A Guzzle client object.
    */
-  public function __construct(Connection $connection, RequestStack $request_stack, ConfigFactoryInterface $config_factory) {
+  public function __construct(Connection $connection, RequestStack $request_stack, ConfigFactoryInterface $config_factory, ClientInterface $http_client) {
     $this->connection = $connection;
     $this->requestStack = $request_stack;
     $this->configFactory = $config_factory;
+    $this->httpClient = $http_client;
   }
 
   /**
@@ -64,13 +76,31 @@ class Resource {
     }
     $base = Environment::getUrlStem();
     $url = $base . $ip . '?' . "api-key=$key";
-    $details = json_decode(file_get_contents($url));
+    $details = json_decode($this->fetchProjectData($url));
 
     $output = [];
     $output['ip'] = $details->ip;
     $output['city'] = $details->city;
     $output['region'] = $details->region;
     return $output;
+  }
+
+  /**
+   * Using http Client to fetch data.
+   */
+  public function fetchProjectData($url) {
+    $method = 'GET';
+    try {
+      $response = $this->httpClient->request($method, $url);
+      $code = $response->getStatusCode();
+      if ($code == 200) {
+        $body = $response->getBody()->getContents();
+        return $body;
+      }
+    }
+    catch(RequestException $e) {
+      watchdog_exception('ip_lookup', $e);
+    }
   }
 
   /**
